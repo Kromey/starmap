98f1db7da4e78472f0693d2d7403ef9cf9324ede
Travis Veazey <travisvz@gmail.com>
2020-03-11 16:53:28 -0800

    Rewrite HyperspaceNetwork to use HyperspaceRoute
    This was a significant rewrite as previously we were using tuples and
    numerous supporting objects, which we have now ripped out in favor of
    our own HyperspaceRoute objects, which now contain all the necessary
    metadata we're currently generating/using about routes.

--- simulation/hyperspace.py
+++ simulation/hyperspace.py
@@ -6,11 +6,7 @@ class HyperspaceNetwork:
     def __init__(self, galaxy, max_dist=7.3, falloff=4.0, stop_cost=0.5, hub_factor=0.5):
         self.__galaxy = galaxy
         self.__routes = []
-        self.__explored = []
-
-        for s in galaxy:
-            self.__routes.append(None)
-            self.__explored.append(False)
+        self.__explored = set()
 
         self.max_dist = max_dist
         self.falloff = falloff
@@ -18,91 +14,85 @@ class HyperspaceNetwork:
         self.hub_factor = hub_factor
 
     def add_star(self, star):
-        self.__explored[self.__galaxy.index(star)] = True
+        self.__explored.add(star)
 
     def discover_route(self):
-        explored = self._get_explored()
-        starti = explored[math.floor(random.random() ** (self.falloff/2) * len(explored))]
-        start = self.__galaxy[starti]
+        explored = list(self.__explored)
+        explored.sort(key=self._get_cost)
+
+        start = explored[math.floor(random.random() ** (self.falloff/2) * len(explored))]
 
         candidates = self._get_candidates(start)
 
-        otheri = candidates[math.floor(random.random() ** self.falloff * len(candidates))]
-        other = self.__galaxy[otheri]
+        other = candidates[math.floor(random.random() ** self.falloff * len(candidates))]
 
         d = start.dist(other)
 
         ab = d * (random.random()*0.5 + 0.75)
         ba = d * (random.random()*0.5 + 0.75)
 
-        print('Yay! Got one! From: ({si}) {sn}; To: ({ei}) {en}'.format(
-            si=starti,
+        print('Yay! Got one! From: {sn}; To: {en}'.format(
             sn=start.name,
-            ei=otheri,
             en=other.name,
         ))
 
-        self._add_route(starti, otheri, ab, ba)
+        route = HyperspaceRoute(start, other, ab, ba)
+        self._add_route(route)
 
     @property
     def routes(self):
-        for a in range(len(self.__routes)):
-            if self.__routes[a] is None:
-                continue
+        for route in self.__routes:
+            yield route
 
-            for b in self.__routes[a]:
-                b = b[0]
-                if b < a:
-                    continue
+    def _get_cost(self, dest, start=None, visited=None):
+        if start is None:
+            start = self.__galaxy[0]
 
-                yield HyperspaceRoute(self.__galaxy[a], self.__galaxy[b])
-
-    def _get_explored(self):
-        explored = []
-
-        for i in range(len(self.__explored)):
-            if self.__explored[i]:
-                cost = self._get_cost(i)
-
-                explored.append((i,cost))
-
-        explored.sort(key=lambda x: x[1])
-
-        return [x[0] for x in explored]
-
-    def _get_cost(self, dest, start=0, visited=None):
         # Stop if we're there
         if dest == start:
             return 0
 
+        # Find all routes from this star
+        routes = []
+        for route in self.__routes:
+            if start in (route.a, route.b):
+                routes.append(route)
+
         # Stop if we have no routes
-        if self.__routes[start] is None:
+        if len(routes) == 0:
             return None
 
-        if visited is None:
-            visited = [start]
-        else:
+        try:
             visited = visited[:]
-            visited.append(start)
+        except TypeError:
+            visited = []
+        visited.append(start)
 
         cost = None
 
-        for nxt in self.__routes[start]:
-            if nxt[0] == dest:
-                return nxt[1]
-            elif nxt[0] not in visited:
+        for route in routes:
+            if route.a == start:
+                other = route.b
+                other_cost = route.ab
+            else:
+                other = route.a
+                other_cost = route.ba
+
+            if other == dest:
+                return other_cost
+            elif other not in visited:
                 if cost is None:
-                    cost = nxt[1]
-                elif nxt[1] > cost:
+                    cost = other_cost
+                elif other_cost > cost:
                     continue
 
-                c = self._get_cost(dest, nxt[0], visited)
+                c = self._get_cost(dest, other, visited)
                 if c is not None:
                     cost = min(cost, c)
 
         if cost:
             cost += self.stop_cost
-            if len(self.__routes[start]) > 3:
+            if len(routes) > 3:
                 cost *= self.hub_factor
 
         return cost
@@ -110,38 +100,54 @@ class HyperspaceNetwork:
     def _get_candidates(self, start):
         candidates = []
 
-        for i in range(len(self.__galaxy)):
-            star = self.__galaxy[i]
-            d = start.dist(star)
+        for other in self.__galaxy:
+            d = start.dist(other)
 
             if d > 0 and d < self.max_dist:
-                candidates.append((i,d))
+                candidates.append(other)
 
-        candidates.sort(key=lambda x: x[1])
+        candidates.sort(key=lambda x: start.dist(x))
 
-        return [candidate[0] for candidate in candidates]
+        return candidates
 
-    def _add_route(self, a, b, ab, ba, bi_d=True):
-        self.add_star(self.__galaxy[a])
+    def _add_route(self, route):
+        self.add_star(route.a)
+        self.add_star(route.b)
 
-        if bi_d:
-            self._add_route(b, a, ba, ab, False)
+        for r in self.__routes:
+            try:
+                r.update(route)
+                return
+            except ValueError:
+                pass
 
-        route = (b, ab)
-        if self.__routes[a] is None:
-            self.__routes[a] = [route]
-        else:
-            for r in range(len(self.__routes[a])):
-                if self.__routes[a][r][0] == b:
-                    if ab < self.__routes[a][r][1]:
-                        self.__routes[a][r] = route
-                    return
-            self.__routes[a].append(route)
+        self.__routes.append(route)
 
 class HyperspaceRoute:
-    def __init__(self, a, b):
+    def __init__(self, a, b, ab, ba):
         self.__a = a
         self.__b = b
+        self.__ab = ab
+        self.__ba = ba
+
+    def update(self, other):
+        if self.a == other.a and self.b == other.b:
+            if other.ab < self.ab:
+                self.__ab = other.ab
+            if other.ba < self.ba:
+                self.__ba = other.ba
+        elif self.a == other.b and self.b == other.a:
+            self.update(other.reverse())
+        else:
+            raise ValueError('Route Mismatch!')
+
+    def reverse(self):
+        return HyperspaceRoute(
+            a=self.b,
+            b=self.a,
+            ab=self.ba,
+            ba=self.ab,
+        )
 
     @property
     def a(self):
@@ -150,4 +156,16 @@ class HyperspaceRoute:
     @property
     def b(self):
         return self.__b
+
+    @property
+    def ab(self):
+        return self.__ab
+
+    @property
+    def ba(self):
+        return self.__ba
+
+    @property
+    def dist(self):
+        return self.a.dist(self.b)
 
