1055ca7bba45b9821037a1e4a3b5c4cc4bb934f1
Travis Veazey <travisvz@gmail.com>
2020-04-01 15:00:44 -0800

    Overwrite existing movie

--- make_movie.sh
+++ make_movie.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-ffmpeg -framerate 60 -i frames/frame-%03d.png -movflags faststart -pix_fmt yuv420p -vf "scale=trunc(iw/2)*2:trunc(ih/2)*2" starmap.mp4
+ffmpeg -framerate 60 -i frames/frame-%03d.png -movflags faststart -pix_fmt yuv420p -vf "scale=trunc(iw/2)*2:trunc(ih/2)*2" starmap.mp4 -y
 
 rm frames/frame-*.png
