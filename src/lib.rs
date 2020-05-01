ed655742706520abf0bdd9e18b3d0ea153560de0
Travis Veazey <travisvz@gmail.com>
2020-05-01 14:47:28 -0800

    Move distance calculation to Point3d method

--- src/lib.rs
+++ src/lib.rs
@@ -9,6 +9,16 @@ pub struct Point3d {
     pub z: f32,
 }
 
+impl Point3d {
+    pub fn distance(&self, other: &Point3d) -> f32 {
+        (
+            (self.x - other.x).powi(2) +
+            (self.y - other.y).powi(2) +
+            (self.z - other.z).powi(2)
+        ).sqrt()
+    }
+}
+
 #[derive(Debug)]
 pub struct Star {
     pub name: String,
--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,4 @@
-use hyperspace::{Record, Star};
+use hyperspace::{Record, Star, Point3d};
 
 
 fn main() {
@@ -7,12 +7,18 @@ fn main() {
     let mut neighbors = 0;
     let mut total = 0;
 
+    let sol = Point3d {
+        x: 0.0,
+        y: 0.0,
+        z: 0.0,
+    };
+
     let mut rdr = csv::Reader::from_path("data/HabHyg.csv").unwrap();
     for result in rdr.deserialize::<Record>() {
         let star = Star::from(result.unwrap());
         total += 1;
 
-        let dist = (star.coords.x.powi(2) + star.coords.y.powi(2) + star.coords.z.powi(2)).sqrt();
+        let dist = star.coords.distance(&sol);
 
         if dist < 17f32 {
             neighbors += 1;
